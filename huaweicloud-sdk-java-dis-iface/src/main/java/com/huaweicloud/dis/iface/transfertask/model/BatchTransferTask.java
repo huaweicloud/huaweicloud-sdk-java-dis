7978fb823ec3144fdbcd4304ab94bbbd13d1a4b7
gaozhiliang1@huawei.com <gaozhiliang1@huawei.com>
2020-05-12 11:14:29 +0800

    dis sdk新增启动和暂停转储任务、通过通道ID查询通道详情功能
    Please enter the commit message for your changes. Lines starting

---
+++ huaweicloud-sdk-java-dis-iface/src/main/java/com/huaweicloud/dis/iface/transfertask/model/BatchTransferTask.java
@@ -0,0 +1,23 @@
+package com.huaweicloud.dis.iface.transfertask.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BatchTransferTask {
+    /**
+     * 转储任务ID
+     */
+    @JsonProperty("id")
+    private String taskId;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+}
---
+++ huaweicloud-sdk-java-dis-iface/src/main/java/com/huaweicloud/dis/iface/transfertask/request/BatchTransferTaskRequest.java
@@ -0,0 +1,56 @@
+package com.huaweicloud.dis.iface.transfertask.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.huaweicloud.dis.iface.transfertask.TransferTaskActionEnum;
+import com.huaweicloud.dis.iface.transfertask.model.BatchTransferTask;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BatchTransferTaskRequest {
+    /**
+     * 转储任务操作，目前支持：<br/>
+     * 1. start：启动转储任务<br/>
+     * 2. stop：停止转储任务
+     */
+    @JsonProperty("action")
+    private String action;
+
+    /**
+     * 转储任务所属通道的名称。
+     */
+    @JsonProperty("stream_name")
+    private String streamName;
+
+    /**
+     * 待操作的转储任务列表。
+     */
+    @JsonProperty("tasks")
+    private List<BatchTransferTask> transferTasks;
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getStreamName() {
+        return streamName;
+    }
+
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+
+    public List<BatchTransferTask> getTransferTasks() {
+        return transferTasks;
+    }
+
+    public void setTransferTasks(List<BatchTransferTask> transferTasks) {
+        this.transferTasks = transferTasks;
+    }
+}
